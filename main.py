29c16d7f52bc3320405b60047ba11e5be09ea698
neyo55 <kbneyo55@gmail.com>
2026-03-18 16:58:35 +0100

    Add Ingress routing

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def read_root():
     hostname = socket.gethostname()
     return {
         "message": "Hello from FastAPI!",
+        "version": "2.0 - The Ingress Update",
         "pod_name": hostname,
         "status": "Unkillable App is Running"
     }
